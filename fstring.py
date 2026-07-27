b72ef6739de8dba5182843225cf6b8b7a1a05577
saikumar <saikumarjella88@gmail.com>
2026-07-27 11:56:55 +0530

    fstring

---
+++ fstring.py
@@ -0,0 +1,12 @@
+name = 'Sai'
+age = 23
+occupation = 'Job'
+
+#print('Hi, my name is '+ name,', my age is'+ age,', my occupation is '+ occupation,'.')
+#print('Hi, my name is '+ name,', my age is '+ str(age),', my occupation is '+ occupation,'.')
+print('Hi, my name is ',name,', my age is',age,', my occupation is ',occupation,'.')
+
+
+#This is f-string, makes coding easier and way more concise and also less error prone
+print(f'Hi, my name is {name}, my age is {age}, my occupation is {occupation}.')
+print(f'my sisters age is {age+2}')
